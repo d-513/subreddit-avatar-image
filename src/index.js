d381ea39aa3674ed104f7354820d10165859e521
Octocat <octocat@github.com>
2020-10-18 16:53:24 +0000

    Fix with standard

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import axios from "axios";
-import cheerio from "cheerio";
+import axios from 'axios'
+import cheerio from 'cheerio'
 
 /**
  * @method
@@ -7,9 +7,9 @@ import cheerio from "cheerio";
  * @returns {string} - The icon URL of the subreddit
  */
 export const getAvatar = async (sub) => {
-  const res = await axios.get(`https://old.reddit.com/r/${sub}/`, {});
-  const $ = cheerio.load(res.data);
-  const img = $("#header-img");
-  const i = img.attr("src") || "//i.imgur.com/bcxwh4B.png";
-  return "https:" + i;
-};
+  const res = await axios.get(`https://old.reddit.com/r/${sub}/`, {})
+  const $ = cheerio.load(res.data)
+  const img = $('#header-img')
+  const i = img.attr('src') || '//i.imgur.com/bcxwh4B.png'
+  return 'https:' + i
+}
