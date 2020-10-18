5f0a04d5d26d586d7ea20161eed248f4c4dfee45
dada513 <kontakt@dada513.eu>
2020-10-18 18:52:13 +0200

    update JSDoc

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import axios from 'axios'
-import cheerio from 'cheerio'
+import axios from "axios";
+import cheerio from "cheerio";
 
 /**
  * @method
+ * @param {string} sub - The subreddit to get the icon from
+ * @returns {string} - The icon URL of the subreddit
  */
 export const getAvatar = async (sub) => {
-  const res = await axios.get(`https://old.reddit.com/r/${sub}/`, {})
-  const $ = cheerio.load(res.data)
-  const img = $('#header-img')
-  const i = img.attr('src') || '//i.imgur.com/bcxwh4B.png'
-  return 'https:' + i
-}
+  const res = await axios.get(`https://old.reddit.com/r/${sub}/`, {});
+  const $ = cheerio.load(res.data);
+  const img = $("#header-img");
+  const i = img.attr("src") || "//i.imgur.com/bcxwh4B.png";
+  return "https:" + i;
+};
